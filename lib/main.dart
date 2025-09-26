82015ff8658f8924fadb8141b8b7de96905f2860
BrunoAlexys <brunoalexysmoura@gmail.com>
2025-09-26 14:11:51 -0300

    wip: login em desenvolvimento

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_comanda/widgets/custom_input.dart';
+import 'package:mobile_comanda/views/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,18 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Comanda Online',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
-      home: Scaffold(
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: CustomInput(
-              hintText: 'Telefone',
-              keyboardType: TextInputType.phone,
-              suffixIcon: Icon(Icons.phone, color: Colors.grey),
-            ),
-          ),
-        ),
-      ),
+      home: const LoginPage(),
     );
   }
 }
